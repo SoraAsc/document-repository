98441f7b1d16a932977fffad73146c0a89198edd
Ivanilso <ivanilsodasilva300@gmail.com>
2023-03-10 10:59:53 -0300

    Inicio do proejto

---
+++ src/main/java/com/project/documentrepository/DocumentRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.project.documentrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/documentrepository/DocumentRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.documentrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
